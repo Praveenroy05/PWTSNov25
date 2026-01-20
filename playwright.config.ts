1b35200a02e0e7a54f980206fc2f2903fe344d7e
Praveen <praveen.k@xnara.com>
2026-01-20 09:06:15 +0530

    modified config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout : 60000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -45,7 +46,7 @@ export default defineConfig({
     trace: 'on',
     screenshot :'on',
     video : 'on',
-    headless : false
+    headless : true
 
     // headless/headed
   },
