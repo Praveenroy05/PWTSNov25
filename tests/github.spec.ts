a3bab796b060e09079f0ac27155c49c860f0d490
Praveen <praveen.k@xnara.com>
2026-01-20 09:17:21 +0530

    added github

--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -1,2 +1,29 @@
 // Github/Bitbucket/Gitlab - Version Control Tool
 
+// master/main/release
+
+// To work with github/bitbucket/gitlab we need to install git in our local system
+// We can download git from https://git-scm.com/install/windows
+
+// If the repository is already available 
+// 1. clone the repository in your local system - git clone repositoryURL
+// 2. Install all the dependencies - npm install
+// 3. Create a new branch - TS01LoginPage - git checkout -b TS01LoginPage
+// 4. Validate if the branch has been created - git branch
+// 5. Make all the changes inside your local system in the newly created branch
+// 6. To check the files which are modifed - git status
+// 7. Add all the changes to the local git - git add .
+// 8. Commit all the changes to the local git - git commit -m "message"
+    // 8.1 - Switch to the main branch and pull all the latest changes - git checkout master - git pull
+    // 8.2 - Switch back to the feature branch  - git checkout TS01LoginPage
+    // 8.3 Mere the master branch changes to your feature branch  - git merge master
+
+// 9. Push all the changes to the remote repository - git push origin TS01LoginPage
+// 10. Go to the https://github.com/Praveenroy05/PWTSNov25 -  yu will see a popup to create a pull request
+// 11. Create a pull request for the changes - assign the reviewer ans create the PR
+// 12. Once the reviewer approves the changes - merge the changes to the main branch
+
+
+// When again you start working on new feature
+// 1. Pull all the lates changes inside your local master branch
+// 2. Repeat #3-#12 everytime
